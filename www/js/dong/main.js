0c6eaed171a0569951899c1df1d76de9d2af06d2
Kasper Gantzhorn <Kasper@Kaspers-MacBook-Pro-2.local>
2017-01-11 09:50:04 +0100

    Bath, Food and Livingroom is done

--- www/js/dong/main.js
+++ www/js/dong/main.js
@@ -9,9 +9,13 @@ var bathLeftBoxState = "in";
 
 $(document).ready(function() {
 	bathInit();
+	initFood();
+	initLivingRoom();
 });
 
 
+
+// BATH
 function bathInit() {
 
 	bathLeftBox = $('#bath-left-box');
@@ -35,6 +39,9 @@ function bathInit() {
 		},
 		onDrag: function() {
 			bathDrag(this);
+		},
+		onDragEnd: function() {
+			bathDragEnd();
 		}
 	});
 
@@ -51,6 +58,12 @@ function bathInit() {
 function bathStartDrag(el) {
 	bathRedBox.removeClass('indicator');
 	arrowGfx.removeClass('arrow-indicator');
+
+	if (bathLeftBoxState == "out") {
+		$('.bath-bubble').removeClass('show');
+	} else {
+		$('#bath-you').addClass('hide');
+	}
 }
 
 function bathRelease(el) {
@@ -62,6 +75,7 @@ function bathRelease(el) {
 	if (bathLeftBoxState == "in") {
 		redLayerIn(el, minX);
 	} else {
+		$('#bath-you').removeClass('hide');
 		redLayerOut(el, maxX);
 	}
 }
@@ -89,6 +103,8 @@ function redLayerIn(el, x) {
 
 	arrowGfx.addClass('rotate');
 	bathLeftBoxState = "out";
+
+	bathDragEnd();
 }
 
 
@@ -96,6 +112,16 @@ function bathDrag(el) {
 	moveVideo(el);
 }
 
+
+function bathDragEnd() {
+	if (bathLeftBoxState == 'out') {
+		$('.bath-bubble').each(function(index) {
+			setTimeout(function() {
+				$('.bath-bubble').eq(index).addClass('show');
+			}, Math.random() * 1000);
+		})
+	}
+}
 
 function moveVideo(el) {
 	var currentVidX = bathLeftBox.position().left;
@@ -106,4 +132,60 @@ function moveVideo(el) {
 	videoElement.css({
 		'left': x
 	});
+}
+
+
+
+// FOOD
+var pct = {
+	num: 0
+};
+
+function initFood() {
+	inView.offset(500);
+	inView('#de-container-food').once('enter', function() {
+		TweenMax.to(pct, 4, {
+			num: "+=29",
+			delay: .7,
+			onStart: showPctCounter,
+			onUpdate: foodUpdateCounter,
+			ease: Expo.easeOut,
+
+		});
+
+		$('.dish-img').addClass('init-dish')
+	});
+}
+
+function foodUpdateCounter() {
+	// var randomNumberTwoDigits = Math.floor(Math.random() * 90 + 10);
+	$('#de-container-food .pct').html(Math.ceil(pct.num) + "%");
+}
+
+function showPctCounter() {
+	$('.food-bubble').addClass('init-bubble');
+	initFoodCTA();
+}
+
+function initFoodCTA() {
+	$('#food-cta').addClass('init-btn');
+}
+
+
+
+//LIVING ROOM
+function initLivingRoom() {
+	inView.offset(500);
+	inView('#de-container-livingroom').once('enter', function() {
+
+		var video = document.getElementById('livingroom-video');
+		video.play()
+		setInterval(initLivingroomCTA, 1000);
+
+	});
+}
+
+
+function initLivingroomCTA() {
+	$('#livingroom-cta').addClass('init-btn');
 }
